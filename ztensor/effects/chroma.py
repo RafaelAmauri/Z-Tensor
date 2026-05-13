c8df00a84971533ed2da88e1f53e28b8922bf12a
RafaelAmauri <amauri.rafael33@gmail.com>
2026-05-13 18:10:55 -0300

    add support for chroma conversion

---
+++ ztensor/effects/chroma.py
@@ -0,0 +1,81 @@
+import torch
+import typing
+
+def bgr2yuv(bgr_video: torch.Tensor) -> torch.Tensor:
+    '''
+    https://www.computerlanguage.com/results.php?definition=YUV%2FRGB+conversion+formulas
+
+    From RGB to YUV
+
+    Y = 0.299R + 0.587G + 0.114B
+    U = 0.492 (B-Y)
+    V = 0.877 (R-Y)
+    '''
+
+    bgr_weights = torch.Tensor([0.114, 0.587, 0.299]).to(bgr_video.device)
+    
+    y = torch.sum(bgr_video * bgr_weights, dim=-1).unsqueeze(-1)
+    u = (0.492 * (bgr_video[:, :, :, 0].unsqueeze(-1) - y))
+    v = (0.877 * (bgr_video[:, :, :, 2].unsqueeze(-1) - y))
+    
+    yuv_video = torch.cat([y, u, v], dim=-1)
+
+    return yuv_video
+
+
+def yuv2bgr(yuv_video: torch.Tensor) -> torch.Tensor:
+    '''
+    https://www.computerlanguage.com/results.php?definition=YUV%2FRGB+conversion+formulas
+
+    From YUV to RGB
+
+    R = Y + (1.140 * V)
+    G = Y - (0.395 * U) - (0.581 * V)
+    B = Y + (2.032 * U)
+    '''
+
+    r = yuv_video[:,:,:, 0] + (1.140 * yuv_video[:,:,:, 2])
+    g = yuv_video[:,:,:, 0] - (0.395 * yuv_video[:,:,:, 1]) - (0.581 * yuv_video[:,:,:, 2])
+    b = yuv_video[:,:,:, 0] + (2.032 * yuv_video[:,:,:, 1])
+
+    r = r.unsqueeze(-1)
+    g = g.unsqueeze(-1)
+    b = b.unsqueeze(-1)
+
+    bgr_video = torch.cat([b, g, r], dim=-1)
+
+    return bgr_video
+
+
+def subsample_chroma_422(yuv_img: torch.Tensor) -> typing.Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    """
+    Applies 422 chroma subsampling
+    """
+    y  = yuv_img[..., 0]
+    uv = yuv_img[..., 1 : ]
+
+    uv = uv.permute(0, -1, 1, 2) # Convert from (frames, h, w, channel) to (frames, channel, h, w)
+
+    pool2d  = torch.nn.AvgPool2d(kernel_size=(1,2), stride=(1,2), padding=0)
+    
+    uv_subsampled = pool2d(uv)
+    uv_subsampled = uv_subsampled.permute(0, 2, 3, 1) # Convert back to (frames, h, w, channel)
+
+    return y, uv_subsampled[..., 0], uv_subsampled[..., 1]
+
+
+def subsample_chroma_420(yuv_img: torch.Tensor) -> typing.Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    """
+    Applies 420 chroma subsampling
+    """    
+    y  = yuv_img[..., 0]
+    uv = yuv_img[..., 1 : ]
+
+    uv = uv.permute(0, -1, 1, 2) # Convert from (frames, h, w, channel) to (frames, channel, h, w)
+
+    pool2d  = torch.nn.AvgPool2d(kernel_size=2, stride=2, padding=0)
+    
+    uv_subsampled = pool2d(uv)
+    uv_subsampled = uv_subsampled.permute(0, 2, 3, 1) # Convert back to (frames, h, w, channel)
+
+    return y, uv_subsampled[..., 0], uv_subsampled[..., 1]
