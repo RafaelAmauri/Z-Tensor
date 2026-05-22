5448e52074777d65cf8fdf847abf47c4a5e91bf4
RafaelAmauri <amauri.rafael33@gmail.com>
2026-05-22 20:52:00 -0300

    added support for new block matching

--- ztensor/codec/encoder.py
+++ ztensor/codec/encoder.py
@@ -5,51 +5,54 @@ import numpy as np
 
 from typing import List
 
+from ztensor.codec import block_matching, padding, serialization
 from ztensor.effects import quantization
-from ztensor.codec import block_matching
+
 
 def encode_video(planes: List[torch.Tensor], i_frame_indices: torch.Tensor, compression_factor: int, num_threads: int, pixel_format: str, quantization_parameter: int) -> bytes:
-    header  = pixel_format.encode('ascii')
-    header += quantization_parameter.to_bytes(1, signed=False)           # uint8  value for the quantization parameter. 1 = Linear (less aggresive), 2 = logarithmic (more aggresive)
-    header += len(planes).to_bytes(1, signed=False)                      # uint8  the number of planes in the video.
-    header += len(i_frame_indices).to_bytes(4, signed=False)             # uint32 the number of i-frames in the video
-    header += i_frame_indices.cpu().numpy().astype(np.uint32).tobytes()  # uint32 indices of the i-frames
 
-    payload = bytes()
+    serialized_video = bytes()
+
+    block_width   = 8
+    search_window = 2
+
+    num_planes = len(planes)
+    num_frames = len(planes[0]) # All planes have the same number of frames, so just take this info from the first plane
+    
+    header  = serialization.serialize_header(pixel_format, 
+                                             quantization_parameter, 
+                                             i_frame_indices, 
+                                             block_width, 
+                                             num_frames,
+                                             num_planes)
+    serialized_video += header
 
     for plane_tensor in planes:
         # Cast to int16 to calculate P-frames. Since the P-frames are only integer values, int16 will do just fine.
-        plane_tensor = plane_tensor.to(torch.int16)
+        plane_tensor  = plane_tensor.to(torch.int16)
 
-        block_matching.block_matching(plane_tensor, 2)
-        raise Exception
+        _, original_plane_h, original_plane_w = plane_tensor.shape
 
-        # Calculate the P-frames
-        p_frames = torch.diff(plane_tensor, dim=0)
-        # Because torch.diff reduces the dimension in 1, we squeeze back a full-zero frame back into the P-frames vector so it keeps the same
-        # shape as the video
-        p_frames = torch.cat([torch.zeros_like(plane_tensor[0]).unsqueeze(0), p_frames])
-    
-        # tells me which frames are P-frames
-        p_frame_mask                  = torch.full(size=(plane_tensor.shape[0],), fill_value=True)
-        p_frame_mask[i_frame_indices] = False
-        
-        # Substitutes the frames in the p_frame_mask for the P-frames calculated with diff
-        plane_tensor[p_frame_mask] = p_frames[p_frame_mask]
+        plane_tensor  = padding.pad_plane(plane_tensor, block_width)
 
-        plane_shape    = np.array(plane_tensor.shape, dtype=np.int32).tobytes()
+        motion_blocks = block_matching.block_matching(plane_tensor, block_width, search_window)
+
+        payload = serialization.serialize_payload(motion_blocks, 
+                                                             plane_tensor, 
+                                                             i_frame_indices, 
+                                                             original_plane_h, 
+                                                             original_plane_w)
+
+        serialized_video += payload
 
         if quantization_parameter :
+            # TODO figure out how to quantize with block-matching.
             plane_tensor = quantization.quantize(plane_tensor, quantization_parameter).to(torch.int8)
         else:
             plane_tensor = plane_tensor.to(torch.uint8)
 
-        plane_bytes    = plane_tensor.cpu().numpy().tobytes()
-        
-        header        += plane_shape # int32 shape for the current plane
-        payload       += plane_bytes # the plane data in bytes
-        
-    compressed_video = compress_video(header + payload, compression_factor, num_threads)
+
+    compressed_video = compress_video(serialized_video, compression_factor, num_threads)
 
     return compressed_video
 
